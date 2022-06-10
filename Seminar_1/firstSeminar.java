eede749553ff370459703adcd6e796f2a8ec81a2
Tina Shumilova <shutiva@gmail.com>
2022-06-10 23:58:13 +0300

    second try

--- Seminar_1/firstSeminar.java
+++ Seminar_1/firstSeminar.java
@@ -1,5 +1,5 @@
 import java.io.FileReader;
-import java.io.BufferedReader;;
+import java.io.BufferedReader;
 /**
 Задача №1
  */
@@ -7,8 +7,8 @@ public class firstSeminar {
     static String[] ReadNumber(String file_name) throws Exception{
         BufferedReader lines = new BufferedReader(new FileReader(file_name));
         String numbers;
-        String number_a = null;
-        String number_b = null;
+        String number_a = "";
+        String number_b = "";
 		while ((numbers = lines.readLine()) != null) {
             char temp = numbers.charAt(0);
             switch (temp) {
@@ -18,7 +18,12 @@ public class firstSeminar {
                     break;
                 case 'b':
                     String temp_b = numbers;
-                    number_b = temp_b.replaceAll("\\D+", "");
+                    int length_b = temp_b.length();
+                    for (int i = 0; i < length_b; i++) {
+                        if (Character.isDigit(temp_b.charAt(i)) || temp_b.charAt(i) == '-'){
+                            number_b = number_b + temp_b.charAt(i);
+                        }
+                    }
                     break;
             }    
 
@@ -26,22 +31,41 @@ public class firstSeminar {
 		lines.close();
         return new String[] {number_a, number_b};
     }
-    static int FindPow(int a, int b){
-        int result = a;
-        for (int i = 0; i < b - 1; i++) {
-            result = result * a;
-        } 
-        return result;
+    static String FindPow(int a, int b){
+        double result = a;
+        String answer = null;
+        if(a == 0){
+           if (b == 0){
+               answer = "не определено";
+           } else {
+               answer = "0";
+           }
+        } else {
+            if (b > 0){
+                for (int i = 0; i < b - 1; i++) {
+                    result = result * a;
+                }
+            } else {
+                if (b == 0){
+                    result = 1;
+                } else {
+                    b = b * -1;
+                    for (int i = 0; i < b - 1; i++) {
+                        result = result * a;
+                    }
+                    result = 1 / result;
+                }    
+            }
+        }
+        answer = Double.toString(result);
+        return answer;
     }
     public static void main(String[] args) throws Exception{
         String[] temp_array = ReadNumber("input.txt");
         int a = Integer.parseInt(temp_array[0]);
         int b = Integer.parseInt(temp_array[1]);
-        int answer = FindPow(a, b);
-        System.out.printf(" %d ^ %d = %d", a, b, answer);
-
-
+        String answer = FindPow(a, b);
+        System.out.printf("a = %d, b = %d \n", a, b);
+        System.out.printf("ответ: %s ", answer);
     }
 }    
-       
-    
