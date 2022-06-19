b1c8c722f172d80d7b8dfa82eb4d6ed302ac66cd
Tina Shumilova <shutiva@gmail.com>
2022-06-19 16:50:31 +0300

    + recurrent

--- Seminar_2/secondHomework.java
+++ Seminar_2/secondHomework.java
@@ -4,12 +4,37 @@
 public class secondHomework {
 
     public static void main(String[] args) {
-        double number = TrippleNimberBasic(4);
-        System.out.println(number);
+        int n = 4;
+        if (n < 0) {
+            System.out.println("Неверное число");
+        }else{
+            //double number = TrippleNimberBasic(n);
+            int number = TrippleNimberRecurrent(n);
+            System.out.println(number);
+        }                   
     }
+        
     static double TrippleNimberBasic(int n){
-        double answer = 0.5 * n  * (n +1);
-        return answer;
+        double answer = 0;
+        if (n==0) {
+            return answer;
+        }else{
+            answer = 0.5 * n  * (n +1);
+            return answer;
+        }
     }
-
+    
+    static int TrippleNimberRecurrent(int n){
+        int result = 0;
+        if (n == 0) {
+            return result;
+        }
+        if (n == 1) {
+            result = 1;
+            return result;
+        }else{
+            result = TrippleNimberRecurrent(n - 1) + n;
+            return result;
+        }
+    }
 }
